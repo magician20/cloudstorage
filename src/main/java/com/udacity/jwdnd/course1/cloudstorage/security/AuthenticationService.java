a72d17530c5c2c23c927e31233bbd23752a1d565
AbdElRahman <am.elsantawy.cs@gmail.com>
2022-10-14 22:48:08 +0300

    securityfix

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/AuthenticationService.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/AuthenticationService.java
@@ -1,7 +1,8 @@
-package com.udacity.jwdnd.course1.cloudstorage.services;
+package com.udacity.jwdnd.course1.cloudstorage.security;
 
 import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
 import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.HashService;
 
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/security/SecurityConfig.java
@@ -1,12 +1,12 @@
-package com.udacity.jwdnd.course1.config;
+package com.udacity.jwdnd.course1.cloudstorage.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -23,16 +23,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(this.authenticationService);
     }
 
+    /*
+     * HttpSecurity object by chaining methods to express security requirements.
+     * 1-define how spring receive the authorization request.
+     * 2-which page it requires authorization to access.
+     * 3-how to handle log outs
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED);
+
+        // Handel access /h2-console
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.ignoringAntMatchers("/h2-console/**"));
+        http.headers(headers -> headers.frameOptions().sameOrigin());
         http.authorizeRequests()
-                .antMatchers("/signup", "/css/**", "/js/**").permitAll()
+                .antMatchers("/h2-console/**", "/signup", "/css/**", "/js/**").permitAll()
+                // Allows authenticated users to make any request that's not explicitly covered
+                // elsewhere.
                 .anyRequest().authenticated();
 
+        // Generates a login form at /login and allows anyone to access it.
         http.formLogin()
                 .loginPage("/login")
                 .permitAll();
-
+        // Redirects successful logins to the /home page
         http.formLogin()
                 .defaultSuccessUrl("/home", true);
 
